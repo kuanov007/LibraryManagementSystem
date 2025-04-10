dd85c46565d47dd9eb33c0b08cb8ca1ccab8f0c2
azamat <kuanovt1@gmail.com>
2025-04-10 16:51:35 +0500

    add functions to LibraryService.java

--- src/main/java/org/example/db/DB.java
+++ src/main/java/org/example/db/DB.java
@@ -1,6 +1,11 @@
 package org.example.db;
 
+import org.example.entities.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface DB {
-    
+    List<Book> books = new ArrayList<>();
 
 }
--- src/main/java/org/example/service/LibraryService.java
+++ src/main/java/org/example/service/LibraryService.java
@@ -1,6 +1,23 @@
 package org.example.service;
 
-public class LibraryService {
+import org.example.entities.Book;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.*;
 
+public interface LibraryService {
+    List<Book> filterBooks(Predicate<Book> predicate);
+
+    List<String> getBookTitles(Function<Book, String> function);
+
+    void printBooks(Consumer<Book> consumer);
+
+    void addBookFromSupplier(Supplier<Book> supplier);
+
+    void updateBookTitle(Book book, UnaryOperator<String> unaryOperator);
+
+    Book findOldestBook(BinaryOperator<Book> binaryOperator);
+
+    List<Book> getSortedBooks(Comparator<Book> comparator);
 }
